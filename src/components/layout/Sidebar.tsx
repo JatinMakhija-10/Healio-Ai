aa2c14bfd62c6d8fc01171a610375f6940d54b0d
JatinMakhija-10 <jatinmakhija2007@gmail.com>
2026-04-05 01:33:46 +0530

    Refactor: Update Sidebar (103/156)

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -14,6 +14,7 @@ import {
     BookOpen,
     Video
 } from "lucide-react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/context/AuthContext";
 import { PHASE_CONFIG } from "@/lib/phaseConfig";
