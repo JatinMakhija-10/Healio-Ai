d37e8eac00943996c4a8c925dd983678a96599d6
JatinMakhija-10 <jatinmakhija2007@gmail.com>
2026-04-05 01:33:34 +0530

    Refactor: Update Checklist (93/156)

--- src/components/dashboard/Checklist.tsx
+++ src/components/dashboard/Checklist.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useState } from "react";
 import { Check, Clock, AlertCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
