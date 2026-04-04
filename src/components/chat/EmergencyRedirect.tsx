43b4d887f87f0b11b275e7715d5d0ed4ddf648c9
JatinMakhija-10 <jatinmakhija2007@gmail.com>
2026-04-05 01:33:29 +0530

    Refactor: Update EmergencyRedirect (88/156)

--- src/components/chat/EmergencyRedirect.tsx
+++ src/components/chat/EmergencyRedirect.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { AlertTriangle, Phone, ExternalLink } from "lucide-react";
 
 interface EmergencyRedirectProps {
